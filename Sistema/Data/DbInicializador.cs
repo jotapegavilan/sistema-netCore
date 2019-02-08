206ec9aa61e9d7a0f5f6894a38f8c77c80ccfe0e
Juan Pablo Gavilan <j.p.daniel.gavilan@gmail.com>
2019-02-08 20:32:27 -0300

    Inicializador de bd, modificación de textos en vistas de categoria.

---
+++ Sistema/Data/DbInicializador.cs
@@ -0,0 +1,32 @@
+﻿using Sistema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Data
+{
+    public class DbInicializador
+    {
+        public static void Inicializar(SistemaContext context)
+        {
+            context.Database.EnsureCreated();
+            //Buscar si existen registros en la tabla categoría
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+            var categorias = new Categoria[]
+            {
+                new Categoria{Nombre="Programacíon",Descripcion="Cursos de programación",Estado=true},
+                new Categoria{Nombre="Ingles",Descripcion="Cursos de Ingles",Estado=true}
+            };
+
+            foreach(Categoria c in categorias)
+            {
+                context.Categoria.Add(c);
+            }
+            context.SaveChanges();
+        }
+    }
+}
